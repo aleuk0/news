eac256a1d823cb13fe3286afacd2c5aaa95dc3d2
Alexander Kozarev <al1102@yandex.ru>
2018-05-09 01:42:45 +0500

    counter

--- news/models.py
+++ news/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
+#минимальные элементы: текст, заголовок, дата отправления, автор, список тегов, количество просмотров
+
 class Post(models.Model): #возможно лучше изменить на Post, а все новости в целом оставить News
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -10,6 +13,7 @@ class Post(models.Model): #возможно лучше изменить на Pos
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    counter = models.IntegerField()
 
     def publish(self):
         self.published_date = timezone.now()
