01657f5dc60d434dde08d2cf66605f4088fdf1ab
Alexander Kozarev <al1102@yandex.ru>
2018-09-01 00:35:48 +0500

    req file

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f$#)kcp88c*rtbk(gboo27%h!(#90ke9_+m76h$qt$qtb3$)z6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', ]
 
 
 # Application definition
