50608e92cdf40f9cab889bc15111f5ade45de2f4
Alexander Kozarev <al1102@yandex.ru>
2018-11-05 17:28:32 +0500

    up

--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, re_path
-from . import views
+from news import views
 
 urlpatterns = [
-	path('', views.news_list, name='news_list'),
+    path('', views.news_list, name='news_list'),
     re_path(r'^news/(?P<pk>\d+)/$', views.news_detail, name='news_detail'),
-	path('news/new/', views.news_new, name='news_new'),
-	re_path(r'^news/(?P<pk>\d+)/edit/$', views.news_edit, name='news_edit'),
-	path('statistics/', views.news_statistics, name='news_statistics'),
+    path('news/new/', views.news_new, name='news_new'),
+    re_path(r'^news/(?P<pk>\d+)/edit/$', views.news_edit, name='news_edit'),
+    path('statistics/', views.news_statistics, name='news_statistics'),
 ]
